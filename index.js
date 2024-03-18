5c6e153d0fc9b0b3e210dad95dd29a7cf64f7fab
Rizzle Ann Mae Santos <rizzleannmaes@gmail.com>
2024-03-18 08:55:23 +0800

    modified javascript

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ let B = false,
     N = false,
     G = false, 
     O = false;
+
+    let BNumber, INumber, NNUmber, GNumber, ONumber;
+    
 function newNumber(maxNumber) {
     // Addition = "+"
     // Subtraction = "-"
@@ -20,18 +23,23 @@ function newNumber(maxNumber) {
    if(x <= 15) {
     console.log(`New number is ${x} belongs to "B"`);
     B = true;
+    BNumber = true;
     } else if(x >=16 && x <= 30) {
         console.log(`New number is ${x} belongs to "I"`);
         I = true;
+        INumber = true;
     } else if(x >=31 && x <= 45) {
         console.log(`New number is ${x} belongs to "N"`);
         N = true;
+        INumber = true;
     } else if(x >=46 && x <= 60) {
         console.log(`New number is ${x} belongs to "G"`);
         G = true;
+        GNumber = true
     } else if(x >=61 && x <= 76) {
         console.log(`New number is ${x} belongs to "O"`);
         O = true;
+        ONumber = true;
     } 
     else {
         console.log(`New number ${x} is invalid.`);
@@ -39,8 +47,34 @@ function newNumber(maxNumber) {
 
     if (B == true && I == true && N == true == G == true && O == true) {
         console.log(`BINGOOOOOO!!!!`);
+        console.table({
+            B: BNumber,
+            I: INumber,
+            N: NNUmber,
+            G: GNumber,
+            O: ONumber,
+        });
     }
 
 }
 
 newNumber(75);
+
+//loops
+//let counter = 5;
+//for(counter; counter >= 0; counter--) {
+  //  console.log(`Current counter is ${counter}`);
+    ////if (counter == 2) break;
+    
+   // newNumber(75);
+//}
+
+//let counter = 10; 
+//while(counter >=0){
+//console.log(`While, Current counter is ${counter}`);
+//counter--;
+ //}
+
+ while (!B || !I || !N|| !G|| !O) {
+    newNumber(75);
+ }
